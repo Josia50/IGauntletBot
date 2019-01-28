c298c4557169a9160bd59a70b9de441f0a5294e9
Josia50 <elwyn.vdb@gmail.com>
2019-01-28 21:12:49 +0100

    First

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const express = require('express');
+const app = express();
+const http = require('http');
+const config = require('./src/config.json')
+const run  = require("./src/run.json")
+
+app.get("/", (request, response) => {
+    console.log(Date.now() + " Ping Received");
+    response.sendStatus(200);
+      });
+     app.listen(process.env.PORT);
+      setInterval(() => {
+        http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+      }, 280000);
+
+
+client.on("ready", () => {
+    console.log('Main Loaded');
+    const welcome = require("./src/welcome.js");
+    const role = require("./src/roleadd.js");
+    const help = require("./src/help.js");
+})
+
+client.login(run.token);
